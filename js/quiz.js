fceeb10f17c30e9d3176486d7a065b1a4be0fad0
Straitsalt <pdsawant2010@gmail.com>
2026-06-30 10:27:07 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,19 @@
+function checkQuiz() {
 
+    let score = 0;
+
+    const answer = document.querySelector('input[name="q1"]:checked');
+
+    if (!answer) {
+        alert("Please select an answer.");
+        return;
+    }
+
+    if (answer.value === "B") {
+        score++;
+    }
+
+    document.getElementById("score").innerHTML =
+        `🎉 Your Score: ${score}/1`;
+
+}
