974b6df4012de9f8a2a4711c3f4a29afbca9799a
Straitsalt <pdsawant2010@gmail.com>
2026-06-30 10:49:56 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCX3eiVX9IRVTBaUw7a-ZiEUyppmKHjAuI",
+    authDomain: "ai-student-hub-1fbcf.firebaseapp.com",
+    projectId: "ai-student-hub-1fbcf",
+    storageBucket: "ai-student-hub-1fbcf.firebasestorage.app",
+    messagingSenderId: "952531943419",
+    appId: "1:952531943419:web:dbb1e4f8f25dc169b0155e",
+    measurementId: "G-YWRY9GNN2C"
+  };
