8d007fd962fc5a16df059b2e9eb451d7da7e67fb
Straitsalt <pdsawant2010@gmail.com>
2026-06-30 10:32:33 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,41 @@
+let seconds = 1500;
+let timer;
 
+function updateDisplay() {
+
+    let min = Math.floor(seconds / 60);
+    let sec = seconds % 60;
+
+    document.getElementById("timer").innerText =
+        `${String(min).padStart(2,"0")}:${String(sec).padStart(2,"0")}`;
+}
+
+function startTimer() {
+
+    clearInterval(timer);
+
+    timer = setInterval(() => {
+
+        if (seconds > 0) {
+            seconds--;
+            updateDisplay();
+        } else {
+            clearInterval(timer);
+            alert("🎉 Great job! Study session completed.");
+        }
+
+    },1000);
+
+}
+
+function resetTimer() {
+
+    clearInterval(timer);
+
+    seconds = 1500;
+
+    updateDisplay();
+
+}
+
+updateDisplay();
